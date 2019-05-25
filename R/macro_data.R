4b590d8400981d7c1f485e8ab31960240be9ed42
Claudius Graebner <claudius@claudius-graebner.com>
2019-05-25 15:04:23 +0200

    Add Chinn Ito data

--- R/macro_data.R
+++ R/macro_data.R
@@ -662,6 +662,39 @@ ameco_full <- ameco_full[, .(year=as.double(as.character(year)),
                          ]
 print("....finished.")
 # TODO check for duplicates
+# Chinn-Ito index==============================================================
+print("Chinn-Ito index...")
+chinn_ito_url <- "http://web.pdx.edu/~ito/kaopen_2016.dta"
+chinn_ito_file <- "data-raw/chinn_ito.csv"
+
+if (download_data | !file.exists((paste0(chinn_ito_file, ".gz")))){
+  tmp <- tempfile(fileext = ".dta")
+  download.file(chinn_ito_url, tmp,
+                quiet = FALSE)
+  chinn_ito_raw <- data.table::as.data.table(haven::read_dta(tmp))
+
+  chinn_ito_raw <- chinn_ito_raw[ccode %in% countries_considered,
+                     .(ccode, year,
+                       kaopen, # Chinn-Ito index
+                       ka_open # Normalized Chinn-Ito index
+                       )
+                     ]
+  data.table::fwrite(chinn_ito_raw, chinn_ito_file)
+  R.utils::gzip(paste0(chinn_ito_file),
+                destname=paste0(chinn_ito_file, ".gz"),
+                overwrite = TRUE)
+} else {
+  chinn_ito_raw <- data.table::fread(paste0(chinn_ito_file, ".gz"))
+}
+chinn_ito <- chinn_ito_raw[,
+                           .(
+                             iso3c = ccode,
+                             year = as.double(year),
+                             chinn_ito = as.double(kaopen),
+                             chinn_ito_normed = as.double(ka_open)
+                           )]
+
+print("finished.")
 
 # KOF Globalization index======================================================
 print("KOF Globalization Index...")
@@ -673,7 +706,6 @@ if (download_data | !file.exists((paste0(kof_file, ".gz")))){
   download.file(kof_url, tmp,
                 quiet = FALSE)
   kof_raw <- data.table::as.data.table(haven::read_dta(tmp))
-  kof_raw[, country:=NULL]
   kof_raw <- kof_raw[code %in% countries_considered,
                      .(code, year,
                        KOFGI, # KOF Globalisation Index
@@ -889,7 +921,7 @@ print("Merging data...")
 macro_data <- Reduce(function(...) merge(..., all=TRUE,
                                          by = c("iso3c", "year")),
                      list(wb_data, swiid_raw, ameco_full, oecd_data,
-                          complexity_data, barro_lee, kof)
+                          complexity_data, barro_lee, kof, chinn_ito)
                      )
 save(macro_data, file = "data/macro_data.rdata")
 macro_data_csv_name <- "data/macro_data.csv"
